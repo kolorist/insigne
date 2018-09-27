ac4890aa7bdefefe9021fcfc8b8eb55f2d5ff044
nhat.nguyenhong@homepc <hongnhatbkhn@gmail.com>
2018-09-27 22:10:32 +0700

    geometry mode render

--- include/insigne/commons.h
+++ include/insigne/commons.h
@@ -10,250 +10,261 @@ namespace insigne {
 
 #define BUFFERED_FRAMES							3
 
-	enum class render_state_togglemask_e {
-		depth_test								= 1u << 0,
-		depth_write								= 1u << 1,
-		cull_face								= 1u << 2,
-		blending								= 1u << 3,
-		scissor_test							= 1u << 4,
-		stencil_test							= 1u << 5
-	};
+enum class render_state_togglemask_e {
+	depth_test								= 1u << 0,
+	depth_write								= 1u << 1,
+	cull_face								= 1u << 2,
+	blending								= 1u << 3,
+	scissor_test							= 1u << 4,
+	stencil_test							= 1u << 5
+};
 
-	enum class draw_type_e {
-		static_surface = 0,
-		dynamic_surface,
-		stream_surface
-	};
+enum class geometry_mode_e : s32 {
+	points = 0,
+	line_strip,
+	line_loop,
+	lines,
+	triangle_strip,
+	triangle_fan,
+	triangles
+};
 
-	enum class compare_func_e {
-		func_never = 0,
-		func_less,
-		func_equal,
-		func_less_or_equal,
-		func_greater,
-		func_not_equal,
-		func_greater_or_equal,
-		func_always
-	};
+enum class draw_type_e {
+	static_surface = 0,
+	dynamic_surface,
+	stream_surface
+};
 
-	enum class front_face_e {
-		face_cw = 0,
-		face_ccw
-	};
+enum class compare_func_e {
+	func_never = 0,
+	func_less,
+	func_equal,
+	func_less_or_equal,
+	func_greater,
+	func_not_equal,
+	func_greater_or_equal,
+	func_always
+};
 
-	enum class face_side_e {
-		front_side = 0,
-		back_side,
-		front_and_back_side
-	};
+enum class front_face_e {
+	face_cw = 0,
+	face_ccw
+};
 
-	enum class blend_equation_e {
-		func_add = 0,
-		func_substract,
-		func_reverse_substract,
-		func_min,
-		func_max
-	};
+enum class face_side_e {
+	front_side = 0,
+	back_side,
+	front_and_back_side
+};
 
-	enum class factor_e {
-		fact_zero = 0,
-		fact_one,
-		fact_src_color,
-		fact_one_minus_src_color,
-		fact_dst_color,
-		fact_one_minus_dst_color,
-		fact_src_alpha,
-		fact_one_minus_src_alpha,
-		fact_dst_alpha,
-		fact_one_minus_dst_alpha,
-		fact_constant_color,
-		fact_one_minus_constant_color,
-		fact_constant_alpha,
-		fact_one_minus_constant_alpha
-	};
+enum class blend_equation_e {
+	func_add = 0,
+	func_substract,
+	func_reverse_substract,
+	func_min,
+	func_max
+};
 
-	enum class operation_e {
-		oper_keep = 0,
-		oper_zero,
-		oper_replace,
-		oper_increment,
-		oper_increment_wrap,
-		oper_decrement,
-		oper_decrement_wrap,
-		oper_invert
-	};
+enum class factor_e {
+	fact_zero = 0,
+	fact_one,
+	fact_src_color,
+	fact_one_minus_src_color,
+	fact_dst_color,
+	fact_one_minus_dst_color,
+	fact_src_alpha,
+	fact_one_minus_src_alpha,
+	fact_dst_alpha,
+	fact_one_minus_dst_alpha,
+	fact_constant_color,
+	fact_one_minus_constant_color,
+	fact_constant_alpha,
+	fact_one_minus_constant_alpha
+};
 
-	enum class data_type_e {
-		elem_unsigned_byte = 0,
-		elem_signed_byte,
-		elem_unsigned_int,
-		elem_signed_int,
-		elem_signed_float,
-		elem_unsigned_int_24_8
-	};
+enum class operation_e {
+	oper_keep = 0,
+	oper_zero,
+	oper_replace,
+	oper_increment,
+	oper_increment_wrap,
+	oper_decrement,
+	oper_decrement_wrap,
+	oper_invert
+};
 
-	enum class material_data_type_e {
-		param_int = 0,
-		param_float,
-		param_sampler2d,
-		param_sampler_cube,
-		param_vec2,
-		param_vec3,
-		param_vec4,
-		param_mat3,
-		param_mat4,
-		param_ub
-	};
+enum class data_type_e {
+	elem_unsigned_byte = 0,
+	elem_signed_byte,
+	elem_unsigned_int,
+	elem_signed_int,
+	elem_signed_float,
+	elem_unsigned_int_24_8
+};
 
-	enum class param_data_type_e {
-		param_int = 0,
-		param_float,
-		param_sampler2d,
-		param_sampler_cube,
-		param_vec2,
-		param_vec3,
-		param_vec4,
-		param_mat3,
-		param_mat4,
-		param_ub
-	};
+enum class material_data_type_e {
+	param_int = 0,
+	param_float,
+	param_sampler2d,
+	param_sampler_cube,
+	param_vec2,
+	param_vec3,
+	param_vec4,
+	param_mat3,
+	param_mat4,
+	param_ub
+};
 
-	struct render_state_t {
-		u32										toggles;
+enum class param_data_type_e {
+	param_int = 0,
+	param_float,
+	param_sampler2d,
+	param_sampler_cube,
+	param_vec2,
+	param_vec3,
+	param_vec4,
+	param_mat3,
+	param_mat4,
+	param_ub
+};
 
-		compare_func_e							depth_func;
+struct render_state_t {
+	u32										toggles;
 
-		front_face_e							front_face;
+	compare_func_e							depth_func;
 
-		blend_equation_e						blend_equation;
-		factor_e								blend_func_sfactor;
-		factor_e								blend_func_dfactor;
+	front_face_e							front_face;
 
-		compare_func_e							stencil_func;
-		u32										stencil_mask;
-		s32										stencil_ref;
-		operation_e								stencil_op_sfail;
-		operation_e								stencil_op_dpfail;
-		operation_e								stencil_op_dppass;
+	blend_equation_e						blend_equation;
+	factor_e								blend_func_sfactor;
+	factor_e								blend_func_dfactor;
 
-		s32										scissor_x;
-		s32										scissor_y;
-		s32										scissor_width;
-		s32										scissor_height;
-	};
+	compare_func_e							stencil_func;
+	u32										stencil_mask;
+	s32										stencil_ref;
+	operation_e								stencil_op_sfail;
+	operation_e								stencil_op_dpfail;
+	operation_e								stencil_op_dppass;
 
-	enum class texture_format_e {
-		rgb = 0,
-		hdr_rgb,
-		srgb,
-		rgba,
-		hdr_rgba,
-		depth,
-		depth_stencil
-	};
+	s32										scissor_x;
+	s32										scissor_y;
+	s32										scissor_width;
+	s32										scissor_height;
+};
 
-	enum class texture_internal_format_e {
-		rg16f = 0,
-		rgb16f,
-		rgba16f,
-		rgb8,
-		rgba8,
-		srgb8,
-		srgb8_alpha8,
-		depth16,
-		depth24,
-		depth24_stencil8
-	};
+enum class texture_format_e {
+	rgb = 0,
+	hdr_rgb,
+	srgb,
+	rgba,
+	hdr_rgba,
+	depth,
+	depth_stencil
+};
 
-	enum class filtering_e {
-		nearest,
-		linear,
-		nearest_mipmap_nearest,
-		linear_mipmap_nearest,
-		nearest_mipmap_linear,
-		linear_mipmap_linear
-	};
+enum class texture_internal_format_e {
+	rg16f = 0,
+	rgb16f,
+	rgba16f,
+	rgb8,
+	rgba8,
+	srgb8,
+	srgb8_alpha8,
+	depth16,
+	depth24,
+	depth24_stencil8
+};
 
-	struct material_param_t {
-		c8										name[128];
-		material_data_type_e					data_type;
+enum class filtering_e {
+	nearest,
+	linear,
+	nearest_mipmap_nearest,
+	linear_mipmap_nearest,
+	nearest_mipmap_linear,
+	linear_mipmap_linear
+};
 
-		material_param_t()
-		{ }
+struct material_param_t {
+	c8										name[128];
+	material_data_type_e					data_type;
 
-		material_param_t(const_cstr i_name, material_data_type_e i_dataType)
-		{
-			strcpy(name, i_name);
-			data_type = i_dataType;
-		}
-	};
+	material_param_t()
+	{ }
 
-	struct shader_param_t {
-		c8										name[128];
-		param_data_type_e						data_type;
+	material_param_t(const_cstr i_name, material_data_type_e i_dataType)
+	{
+		strcpy(name, i_name);
+		data_type = i_dataType;
+	}
+};
 
-		shader_param_t()
-		{ }
+struct shader_param_t {
+	c8										name[128];
+	param_data_type_e						data_type;
 
-		shader_param_t(const_cstr i_name, param_data_type_e i_dataType)
-			: data_type(i_dataType)
-		{
-			strcpy(name, i_name);
-		}
-	};
+	shader_param_t()
+	{ }
 
-	struct color_attachment_t {
-		c8										name[128];
-		texture_format_e						texture_format;
-		
-		color_attachment_t()
-		{ }
+	shader_param_t(const_cstr i_name, param_data_type_e i_dataType)
+		: data_type(i_dataType)
+	{
+		strcpy(name, i_name);
+	}
+};
 
-		color_attachment_t(const_cstr i_name, const texture_format_e i_texFormat)
-			: texture_format(i_texFormat)
-		{
-			strcpy(name, i_name);
-		}
-	};
+struct color_attachment_t {
+	c8										name[128];
+	texture_format_e						texture_format;
+	
+	color_attachment_t()
+	{ }
 
-	typedef floral::fixed_array<material_param_t, freelist_allocator_t>	material_param_list_t;
-	typedef floral::fixed_array<shader_param_t, arena_allocator_t>		shader_param_list_t;
-	typedef floral::fixed_array<color_attachment_t, arena_allocator_t>	color_attachment_list_t;
+	color_attachment_t(const_cstr i_name, const texture_format_e i_texFormat)
+		: texture_format(i_texFormat)
+	{
+		strcpy(name, i_name);
+	}
+};
 
-	typedef s32									shader_handle_t;
-	typedef s32									texture_handle_t;
-	typedef s32									surface_handle_t;
-	typedef s32									material_handle_t;
-	typedef s32									framebuffer_handle_t;
-	typedef s32									param_id;
-	typedef s32									color_attachment_id;
-	typedef s32									ub_handle_t;
+typedef floral::fixed_array<material_param_t, freelist_allocator_t>	material_param_list_t;
+typedef floral::fixed_array<shader_param_t, arena_allocator_t>		shader_param_list_t;
+typedef floral::fixed_array<color_attachment_t, arena_allocator_t>	color_attachment_list_t;
 
-	//------------------------------------------
-	struct framebuffer_descriptor_t {
-		s32										width, height;
-		f32										scale;
-		bool									has_depth;
-		color_attachment_list_t*				color_attachments;
-	};
+typedef s32									shader_handle_t;
+typedef s32									texture_handle_t;
+typedef s32									surface_handle_t;
+typedef s32									material_handle_t;
+typedef s32									framebuffer_handle_t;
+typedef s32									param_id;
+typedef s32									color_attachment_id;
+typedef s32									ub_handle_t;
 
-	struct surface_descriptor_t {
-		size									vertex_stride;
-		s32										vertices_count;
-		s32										indices_count; // indices is always 'unsigned int'
-	};
+//------------------------------------------
+struct framebuffer_descriptor_t {
+	s32										width, height;
+	f32										scale;
+	bool									has_depth;
+	color_attachment_list_t*				color_attachments;
+};
 
-	struct texture2d_descriptor_t {
-		s32										width, height;
-		texture_format_e						format;
-		filtering_e								min_filter, mag_filter;
-		bool									has_mipmap;
-	};
+struct surface_descriptor_t {
+	size									vertex_stride;
+	s32										vertices_count;
+	s32										indices_count; // indices is always 'unsigned int'
+};
 
-	struct texturecube_descriptor_t {
-		s32										face_width, face_height;
-		texture_format_e						format;
-		filtering_e								min_filter, mag_filter;
-		bool									has_mipmap;
-	};
+struct texture2d_descriptor_t {
+	s32										width, height;
+	texture_format_e						format;
+	filtering_e								min_filter, mag_filter;
+	bool									has_mipmap;
+};
+
+struct texturecube_descriptor_t {
+	s32										face_width, face_height;
+	texture_format_e						format;
+	filtering_e								min_filter, mag_filter;
+	bool									has_mipmap;
+};
+
 }
--- include/insigne/renderer_gl.hpp
+++ include/insigne/renderer_gl.hpp
@@ -5,54 +5,63 @@
 namespace insigne {
 namespace renderer {
 
-	template <typename t_surface>
-	void draw_surface_idx(const surface_handle_t& i_surfaceHdl, const material_t& i_matSnapshot,
-			const s32 i_segSize, const voidptr i_segOffset)
+template <typename t_surface>
+void draw_surface_idx(const surface_handle_t& i_surfaceHdl, const material_t& i_matSnapshot,
+		const s32 i_segSize, const voidptr i_segOffset)
+{
+	PROFILE_SCOPE(draw_surface_idx);
+
+	detail::surface surf = detail::s_surfaces[i_surfaceHdl];
+	detail::shader& shdr = detail::s_shaders[i_matSnapshot.shader_handle];
+
+	s32 iCount = 0;
+	if (i_segSize < 0) iCount = surf.icount; else iCount = i_segSize;
+
 	{
-		PROFILE_SCOPE(draw_surface_idx);
-
-		detail::surface surf = detail::s_surfaces[i_surfaceHdl];
-		detail::shader& shdr = detail::s_shaders[i_matSnapshot.shader_handle];
-
-		s32 iCount = 0;
-		if (i_segSize < 0) iCount = surf.icount; else iCount = i_segSize;
-
-		{
-			PROFILE_SCOPE(update_shader);
-			pxUseProgram(shdr.gpu_handle);
-			// setup material
-			for (u32 i = 0; i < i_matSnapshot.float_params.get_size(); i++)
-				pxUniform1f(shdr.float_params[i], i_matSnapshot.float_params[i].value);
-			for (u32 i = 0; i < i_matSnapshot.vec3_params.get_size(); i++)
-				pxUniform3fv(shdr.vec3_params[i], 1, &i_matSnapshot.vec3_params[i].value.x);
-			for (u32 i = 0; i < i_matSnapshot.mat4_params.get_size(); i++)
-				pxUniformMatrix4fv(shdr.mat4_params[i], 1, GL_FALSE, &(i_matSnapshot.mat4_params[i].value[0][0]));
-			s32 activeTextureSlot = 0;
-			for (u32 i = 0; i < i_matSnapshot.texture2d_params.get_size(); i++) {
-				detail::texture& tex = detail::s_textures[static_cast<s32>(i_matSnapshot.texture2d_params[i].value)];
-				pxActiveTexture(GL_TEXTURE0 + activeTextureSlot);
-				pxBindTexture(GL_TEXTURE_2D, tex.gpu_handle);
-				pxUniform1i(shdr.texture2d_params[i], activeTextureSlot);
-				activeTextureSlot++;
-			}
-			for (u32 i = 0; i < i_matSnapshot.texturecube_params.get_size(); i++) {
-				detail::texture& tex = detail::s_textures[static_cast<s32>(i_matSnapshot.texturecube_params[i].value)];
-				pxActiveTexture(GL_TEXTURE0 + activeTextureSlot);
-				pxBindTexture(GL_TEXTURE_CUBE_MAP, tex.gpu_handle);
-				pxUniform1i(shdr.texture_cube_params[i], activeTextureSlot);
-				activeTextureSlot++;
-			}
+		pxUseProgram(shdr.gpu_handle);
+		// setup material
+		for (u32 i = 0; i < i_matSnapshot.float_params.get_size(); i++)
+			pxUniform1f(shdr.float_params[i], i_matSnapshot.float_params[i].value);
+		for (u32 i = 0; i < i_matSnapshot.vec3_params.get_size(); i++)
+			pxUniform3fv(shdr.vec3_params[i], 1, &i_matSnapshot.vec3_params[i].value.x);
+		for (u32 i = 0; i < i_matSnapshot.mat4_params.get_size(); i++)
+			pxUniformMatrix4fv(shdr.mat4_params[i], 1, GL_FALSE, &(i_matSnapshot.mat4_params[i].value[0][0]));
+		s32 activeTextureSlot = 0;
+		for (u32 i = 0; i < i_matSnapshot.texture2d_params.get_size(); i++) {
+			detail::texture& tex = detail::s_textures[static_cast<s32>(i_matSnapshot.texture2d_params[i].value)];
+			pxActiveTexture(GL_TEXTURE0 + activeTextureSlot);
+			pxBindTexture(GL_TEXTURE_2D, tex.gpu_handle);
+			pxUniform1i(shdr.texture2d_params[i], activeTextureSlot);
+			activeTextureSlot++;
 		}
-
-		// draw
-		pxBindBuffer(GL_ARRAY_BUFFER, surf.vbo);
-		pxBindBuffer(GL_ELEMENT_ARRAY_BUFFER, surf.ibo);
-		t_surface::describe_vertex_data();
-		{
-			PROFILE_SCOPE(issue_draw_call);
-			pxDrawElements(GL_TRIANGLES, iCount, GL_UNSIGNED_INT, i_segOffset);
+		for (u32 i = 0; i < i_matSnapshot.texturecube_params.get_size(); i++) {
+			detail::texture& tex = detail::s_textures[static_cast<s32>(i_matSnapshot.texturecube_params[i].value)];
+			pxActiveTexture(GL_TEXTURE0 + activeTextureSlot);
+			pxBindTexture(GL_TEXTURE_CUBE_MAP, tex.gpu_handle);
+			pxUniform1i(shdr.texture_cube_params[i], activeTextureSlot);
+			activeTextureSlot++;
 		}
 	}
+
+	// draw
+	pxBindBuffer(GL_ARRAY_BUFFER, surf.vbo);
+	pxBindBuffer(GL_ELEMENT_ARRAY_BUFFER, surf.ibo);
+
+	static GLenum s_geometryMode[] = {
+		GL_POINTS,
+		GL_LINE_STRIP,
+		GL_LINE_LOOP,
+		GL_LINES,
+		GL_TRIANGLE_STRIP,
+		GL_TRIANGLE_FAN,
+		GL_TRIANGLES
+	};
+
+	t_surface::describe_vertex_data();
+	{
+		pxDrawElements(s_geometryMode[s32(t_surface::s_geometry_mode)], iCount, GL_UNSIGNED_INT, i_segOffset);
+	}
+}
 
 }
 }
