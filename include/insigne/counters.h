67f92d1a364891c980861141804b158eacd54f25
Nhat Nguyen Hong <nhat.nguyenhong@gameloft.com>
2020-11-28 10:10:49 +0700

    add tiles counter

--- include/insigne/counters.h
+++ include/insigne/counters.h
@@ -58,6 +58,7 @@ struct hardware_counters
 	f32											fragment_cycles[debug_frames_count];
 	f32											tiler_cycles[debug_frames_count];
 	f32											frag_elim[debug_frames_count];
+	f32											tiles[debug_frames_count];
 
 	f32											shader_texture_cycles[debug_frames_count];
 	f32											varying_16_bits[debug_frames_count];
--- src/system.cpp
+++ src/system.cpp
@@ -116,6 +116,7 @@ void dispatch_frame()
 			ctBuffer.fragment_cycles = g_hardware_counters->fragment_cycles;
 			ctBuffer.tiler_cycles = g_hardware_counters->tiler_cycles;
 			ctBuffer.frag_elim = g_hardware_counters->frag_elim;
+			ctBuffer.tiles = g_hardware_counters->tiles;
 			ctBuffer.shader_texture_cycles = g_hardware_counters->shader_texture_cycles;
 			ctBuffer.varying_16_bits = g_hardware_counters->varying_16_bits;
 			ctBuffer.varying_32_bits = g_hardware_counters->varying_32_bits;
